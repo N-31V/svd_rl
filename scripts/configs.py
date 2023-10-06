ce2832d301916a00c83105a141b10d2089a42014
N-31.V <litvintseva.ann@gmail.com>
2023-10-06 17:21:49 +0300

    some configs added

--- scripts/configs.py
+++ scripts/configs.py
@@ -21,6 +21,19 @@ CONFIGS = {
         start_epoch=10,
         epsilon_step=10**-3
     ),
+    'light_pruning_epoch': Config(
+        name='light_pruning_epoch',
+        actions=[Actions.train_decompose, Actions.prune_99, Actions.prune_9],
+        state_mask=['f1', 'size', 'epoch'],
+        start_epoch=10,
+        epsilon_step=10 ** -3
+    ),
+    'simple_pruning_epoch_hoer': Config(
+        name='simple_pruning_epoch_hoer',
+        actions=[Actions.train_decompose, Actions.prune_9, Actions.increase_hoer, Actions.decrease_hoer],
+        state_mask=['f1', 'size', 'epoch', 'hoer_factor'],
+        start_epoch=10,
+    ),
     'simple_dec': Config(
         name='simple_dec',
         actions=[Actions.train_compose, Actions.train_decompose, Actions.prune_9],
