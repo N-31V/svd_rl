ce8ae59a3c53377088a89b24fbac92bfb838d5eb
N-31.V <litvintseva.ann@gmail.com>
2023-11-14 18:52:00 +0300

    Added ImageNet config

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,5 +1,7 @@
 """The module contains experiment configurations."""
 from functools import partial
+from torchvision.datasets import ImageNet
+from torchvision.models import resnet18, ResNet18_Weights
 from svdtrainer.config import Config
 from svdtrainer.state import NState
 from svdtrainer.actions import ActionConverter, Actions
@@ -25,5 +27,24 @@ CONFIGS = {
         model=resnet20,
         weights='/home/n31v/workspace/svd_rl/scripts/models/CIFAR10/ResNet/20_CosineAnnealingLR/train.sd.pt',
         f1_baseline=0.928,
+    ),
+    'imagenet_light_x3': Config(
+        name='imagenet_light_x3',
+        actions=ActionConverter(
+            actions=[Actions.stop, Actions.train, Actions.channel, Actions.spatial, Actions.prune_999, Actions.prune_99]
+        ),
+        state=partial(NState, n=3),
+        train_ds=ImageNet(
+            root='/media/n31v/data/datasets/ImageNet',
+            transform=ResNet18_Weights.IMAGENET1K_V1.transforms()
+        ),
+        val_ds=ImageNet(
+            root='/media/n31v/data/datasets/ImageNet',
+            split='val',
+            transform=ResNet18_Weights.IMAGENET1K_V1.transforms()
+        ),
+        model=partial(resnet18, weights='DEFAULT'),
+        weights=None,
+        f1_baseline=0.6976
     ),
 }
